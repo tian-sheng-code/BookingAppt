e725f63353122b53db112a07c7af3aa92f642bce
tian-sheng-code <sheng3212324@gmail.com>
2021-05-28 18:43:59 +0800

    Initial Commit

---
+++ Shared/BookingApptApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookingApptApp.swift
+//  Shared
+//
+//  Created by Gue Li Ling on 28/5/21.
+//
+
+import SwiftUI
+
+@main
+struct BookingApptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
